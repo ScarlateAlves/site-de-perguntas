97bceb09c1d2ba37b7bcad7fbf214afc57187f1a
Scarlate <scarlate100@gmail.com>
2022-04-09 17:39:03 -0300

    feat: create hook use auth

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,9 @@
+import React, { useContext } from 'react'
+
+import { AuthContext } from '../contexts/Authcontext'
+
+export function useAuth() {
+  const value = useContext(AuthContext)
+
+  return value
+}
